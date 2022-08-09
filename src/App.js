d1112cfe48b72e712a3334ce4f7c69b2274821d5
hajamor@gmail.com <kawther-hajamor@DOM-SIRAIL.SIRAIL.FR>
2022-08-09 13:16:05 +0100

    SSTT

--- src/App.js
+++ src/App.js
@@ -1,53 +1,67 @@
 import './App.css';
-import { Component } from 'react';
+import React, { Component } from 'react';
+import Fullname from './Component/Profile/FullName';
 
 
 class App extends Component {
 
-
-  
-  constructor(){
+  constructor() {
     super()
-  this.user={
-    name:'kawther',
-    counter : "0" ,
-    show : false 
+    this.state = {
+      name: 'kawther',
+      counter: "0",
+      show: false
+    }
   }
-  }
-  Incerement (){
+  Incerement() {
     this.setuser({
-      counter: this.this.user.counter+1
+      counter: this.this.user.counter + 1
     })
   }
-  Decrement = () =>{
-    if(this.state.counter>0){
+  Decrement = () => {
+    if (this.state.counter > 0) {
       this.setuser({
-        counter : this.state.counter - 1
+        counter: this.state.counter - 1
       })
     }
+  }
+Reset =()=>{
+this.setState({
+  counter : 0
+})
 
-
-
-
-
-
-
-  return (
-    <div className='App'>
-    
-    <h1> WS KHA </h1>
-       <h2> {name}</h2>
-       <hr/>
-       <h2> {this.user.counter}</h2>
-       <button onClick={()=> this.Incerement()}>+</button>
-        
-    <p> Bonjour </p>
-  </div>  
-
-
-
-  );
 }
+Incerement_with_value =(val) =>{
+  this.setState({
+    counter : this.state.counter + val
+  })
 }
 
-export default App;
+
+
+
+    render(){
+
+      let name = this.state.name
+
+
+      return (
+        <div className='App'>
+
+          <h1> WS KHA </h1>
+          <h2> {name}</h2>
+          <hr />
+          <h2> {this.state.counter}</h2>
+          <button onClick={() => this.Incerement()}>+</button>
+          <button onClick={ () => this.Decrement()}>-</button>
+          <button onClick={() => this.Reset ()}>Reset</button >
+
+          <p> Bonjour </p>
+        </div>
+
+      );
+
+    }
+  }
+
+    export default App;
--- src/Component/Profile/FullName.js
+++ src/Component/Profile/FullName.js
@@ -1,21 +1,37 @@
 import React, {Component} from 'react'
 
-export default class Fullname exctends component {
+ class Fullname extends Component {
 
 constructor() {
 
 console.log ('constructor')
 super();
-this.name= {
-timer :0,
-intervall: null
+this.state = {
+Fullname :'',
+intervall: null,
+timer:0
+}
+}
 
+componentDidCatch(){
+    console.log('timer is mounted')
+    this.setState({
+     intervall : setInterval(()=> this.setState({
+        timer: this.state.timer +1
+     }),500)
+    })
 }
 
 
+render(){
+    return(
+        <div>
+            <h2> Timer </h2>
+            <h3> HELOO</h3>
 
-
-
-
+        </div>
+    )
 }
 }
+
+export default Fullname
